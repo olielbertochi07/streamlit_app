df2d8556e964963ff989fd8df5f87226383dfbea
olielbertochi07 <62772807+olielbertochi07@users.noreply.github.com>
2024-02-28 20:28:29 -0300

    Update app_lista_produtos.py

--- app_lista_produtos.py
+++ app_lista_produtos.py
@@ -2,7 +2,6 @@ import base64
 import streamlit as st
 from app_functions import get_filtered_data
 from db_functions import get_database_connection
-
 def Listar_Produtos():
     st.subheader("Lista de Productos")
     st.subheader("")
@@ -18,17 +17,13 @@ def Listar_Produtos():
             st.markdown('<hr class="linha-presupuesto">', unsafe_allow_html=True)
             st.markdown("<style> .linha-presupuesto {margin: 0px 0;}</style>",unsafe_allow_html=True)
         left_column, right_column = st.columns([1, 3])
-
         btnImg = st.button(f"Imagen Nº: {row["Id"]}")
         st.text("")
         if btnImg:   
             e = row["Id"] 
         else:
             e = 0
-
         with left_column:
-
-
             id_format = (f'ID: {row["Id"]}')
             codigo_format = (f'{row["Codigo"]}')
             st.markdown(f'<div style="float: left;">{id_format}</div>', unsafe_allow_html=True)
@@ -46,15 +41,15 @@ def Listar_Produtos():
             vent_formatted ="Venta: " + "{:,} Gs".format(int(row['Venta'])).replace(",", ".")     
             st.markdown(f'<div style="float: left;">{may_formatted}</div><div style="float: right;">{vent_formatted}</div>', unsafe_allow_html=True)     
         if e > 0:
-                with st.form(key='frm_img'):
-                    st.markdown(row["Descripcion"])   
-                    mydb = get_database_connection() 
-                    myc = mydb.cursor()
-                    SQL = "SELECT img, n_img FROM v_app_lista_imagens_produtos WHERE id_producto = %s;"
-                    myc.execute(SQL, (row["Id"],))
-                    images = myc.fetchall()
-                    for img_data in images:
-                        encoded_img = base64.b64encode(img_data[0]).decode("utf-8")
-                        img_tag = f'<img src="data:image/png;base64,{encoded_img}" alt="product_image" style="max-width:100%; margin-bottom: 10px;">'
-                        st.markdown(img_tag, unsafe_allow_html=True)
-                    submit_button = st.form_submit_button(label='Volver')     
+            with st.form(key='frm_img'):
+                st.markdown(row["Descripcion"])   
+                mydb = get_database_connection() 
+                myc = mydb.cursor()
+                SQL = "SELECT img, n_img FROM v_app_lista_imagens_produtos WHERE id_producto = %s;"
+                myc.execute(SQL, (row["Id"],))
+                images = myc.fetchall()
+                for img_data in images:
+                    encoded_img = base64.b64encode(img_data[0]).decode("utf-8")
+                    img_tag = f'<img src="data:image/png;base64,{encoded_img}" alt="product_image" style="max-width:100%; margin-bottom: 10px;">'
+                    st.markdown(img_tag, unsafe_allow_html=True)
+                submit_button = st.form_submit_button(label='Volver')     
